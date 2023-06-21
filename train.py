ea93614a8735e2e2463191d47e405d6f34f9a610
Dmitry-Konovalov <89130809848@mail.ru>
2023-06-21 10:24:22 +0300

    stage1.7

--- train.py
+++ train.py
@@ -5,6 +5,14 @@ import pandas as pd
 import boto3
 from dotenv import load_dotenv
 
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import r2_score
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import balanced_accuracy_score, recall_score, precision_score, f1_score
+from sklearn.model_selection import train_test_split
+
+
 def read_df_from_s3(bucket='datasets', file_name='winequality-red.csv'):
     s3 = boto3.resource(
         's3',
@@ -18,9 +26,29 @@ def read_df_from_s3(bucket='datasets', file_name='winequality-red.csv'):
 
     return df 
 
+def train(df):
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(['quality'], axis=1),
+                                                    df['quality'], test_size=0.30,
+                                                    random_state=42)
+        
+    model_1 = RandomForestClassifier().fit(X_train, y_train)
+    model_1_pred = model_1.predict(X_test)
+
+    balanced_accuracy = balanced_accuracy_score(y_test, model_1_pred)
+    recall = recall_score(y_test, model_1_pred, average='weighted')
+    precision = precision_score(y_test, model_1_pred, average='weighted')
+    f1 = f1_score(y_test, model_1_pred, average='weighted')
+    f2 = r2_score(y_test, model_1_pred)
+    print('Balanced accuracy: ', balanced_accuracy)
+    print('Recall: ', recall)
+    print('Precision: ', precision)
+    print('F1-score: ', f1)
+    print("r2:", f2)  
 
 if __name__ == '__main__':
     load_dotenv()
-    
+
     df = read_df_from_s3()
     print(df.head())
+
+    train(df)
